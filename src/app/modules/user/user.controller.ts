d861baeadbb5484740106c3237a7638a67a87181
Obaidullah-ahsan <abir60303@gmail.com>
2025-07-12 14:51:45 +0600

    complete part 1

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     // throw new AppError(httpStatus.BAD_REQUEST, 'fake error')
+//     const user = await UserServices.createUser(req.body);
+
+//     res.status(httpStatus.CREATED).json({
+//       message: "User created successfully",
+//       user,
+//     });
+//   } catch (err: any) {
+//     // eslint-disable-next-line no-console
+//     console.log(err);
+//     next(err);
+//   }
+// };
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserServices.createUser(req.body);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User created successfully",
+      data: user,
+    });
+  }
+);
+
+const getAllUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await UserServices.getAllUser();
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "All User Retrieved Successfully",
+      data: result,
+    });
+  }
+);
+
+export const UserController = {
+  createUser,
+  getAllUser,
+};
